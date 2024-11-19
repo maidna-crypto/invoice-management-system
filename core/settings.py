a38e8a1d8a5416ad7faa34f3bac6b8fa3d8e3224
ma.narouei1@gmail.com <ma.narouei1@gmail.com>
2024-11-19 13:33:34 +0330

    static settings

--- core/settings.py
+++ core/settings.py
@@ -122,8 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where static files will be collected
 
-STATIC_URL = 'static/'
+# For development and production, specify where to find additional static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

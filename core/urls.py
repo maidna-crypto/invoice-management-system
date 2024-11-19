a760e01003eb49462052a98ad0ae8c3757f43cec
ma.narouei1@gmail.com <ma.narouei1@gmail.com>
2024-11-19 14:00:09 +0330

    static url settings in urls.py added

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -27,5 +29,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/invoice/', include(invoice_urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
